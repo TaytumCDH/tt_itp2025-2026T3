cfe2a6c73f162c379b9b7855e3aca2e567664b8a
TaytumCDH <Thioulon.Taytum.29@c-dh.org>
2026-04-09 19:31:05 +0000

    14movies

--- 15_alpha_name.py
+++ 15_alpha_name.py
@@ -1,7 +1,2 @@
 alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-print(alpha[19])
-print(alpha[0])
-print(alpha[24])
-print(alpha[19])
-print(alpha[20])
-print(alpha[12])
+print(alpha[19] + alpha[0] + alpha[24] + alpha[19] + alpha[20] + alpha[12])
