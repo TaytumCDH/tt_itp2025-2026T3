e6b142505960cc3a7a40672976381d2ea0409a62
TaytumCDH <Thioulon.Taytum.29@c-dh.org>
2026-04-15 19:48:12 +0000

    ghjkl;'

---
+++ 14_favorite_movies.py
@@ -0,0 +1,2 @@
+FavoriteMovies=['Avengers Endgame', 'A minecraft movie', 'Spider-Man: Across the Spider-Verse', 'Spider-Man: Into the Spider-Verse', 'Avengers: Infinity War']
+print("My favorite movies are" + str(FavoriteMovies))
