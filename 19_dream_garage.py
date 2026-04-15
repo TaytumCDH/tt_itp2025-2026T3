2e383eff9d895449cd57874e356f11f4e4b4156c
TaytumCDH <Thioulon.Taytum.29@c-dh.org>
2026-04-15 19:48:20 +0000

    dfm

---
+++ 19_dream_garage.py
@@ -0,0 +1,4 @@
+dream_garage=['A dimaond car','Klein Vision AirCar','Batmobile',' Yangwang U9 Extreme',' Mercedes-Benz Vision AVTR']
+print('The cars in my garage are'+str(dream_garage))
+currently_driving = dream_garage.pop(1)
+print('i am currently driving' + currently_driving + 'over the sea to hawai')
